bd54781940f0e8b5eda32f0a60132bb5d94d2b03
Jun Seo <tgw4460@naver.com>
2018-05-22 20:42:14 +0900

    Refactoring using def()

--- run.py
+++ run.py
@@ -6,97 +6,107 @@ import datetime
 import requests
 import util
 import urllib
+import konlpy
 from bs4 import BeautifulSoup
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#"/Users/jun/PycharmProjects/stockCrawler/(20180522)'거래실종', 숨죽인 서울 아파트 시장.txt")
-
 
 #============================ Stock Notifier ============================
-url ="http://finance.daum.net/item/quote.daum?code=068270"
+def stock_notifier():
+    url ="http://finance.daum.net/item/quote.daum?code=068270"
 
-response = urllib.urlopen(url)
-bot = telegram.Bot(token = util.my_token)
-updates = bot.getUpdates()
-for u in updates :
-    print(u.message)
+    response = urllib.urlopen(url)
+    soup = BeautifulSoup(response, 'html.parser')
 
-soup = BeautifulSoup(response, 'html.parser')
+    price = soup.find('td', {'class': 'num quoteFst cUp'}).string
+    print('celtrion : ' + price)
 
-price = soup.find('td', {'class': 'num quoteFst cUp'}).string
+    #==send Message==
+    send_telegram('celtrion : ' + price)
 
-print('celtrion : ' + price)
-
-#send Message
-#bot.sendMessage(chat_id = '@peries_bot', text='celtrion : ' + price)
-
-
-
-dt = datetime.datetime.now()
-today = dt.strftime("%Y%m%d")
-if not os.path.exists('articles/' + today):
-    os.makedirs('articles/' + today)
 
 #==============================Real Estat News Notifier ==============================
-
 #Today's real estate news. It's gonna be called in 11:30 every day.
+def land_news_notifier():
+    dt = datetime.datetime.now()
+    today = dt.strftime("%Y%m%d")
+    if not os.path.exists('articles/' + today):
+        os.makedirs('articles/' + today)
 
-partialUrl = "http://land.naver.com/news/headline.nhn?bss_ymd=" + today
-response = urllib.urlopen(partialUrl)
-soup = BeautifulSoup(response, 'html.parser')
-links_with_text = []
+    partialUrl = "http://land.naver.com/news/headline.nhn?bss_ymd=" + today
+    response = urllib.urlopen(partialUrl)
+    soup = BeautifulSoup(response, 'html.parser')
+    links_with_text = []
 
-for a in soup.find_all('a', href=True):
-    if a.text:
-        links_with_text.append(a['href'])
+    for a in soup.find_all('a', href=True):
+        if a.text:
+            links_with_text.append(a['href'])
 
-#bot.sendMessage(chat_id = util.my_chat_id, text = "Hello. " + "Belows are " + today + "'s naver real estate news")
+    send_telegram("Hello. " + "Belows are " + today + "'s naver real estate news")
 
-for url in links_with_text:
-    if "/news/newsRead.nhn?type=headline" in url:
-        print('land.naver.com' + url)
-        page = requests.get('http://land.naver.com' + url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        header = soup.find('div', {'class' : 'article_header'}).find('h3').get_text(strip=True)
-        print(header)
-        bodyContents = soup.find('div', {'id' : 'articleBody'}).get_text(strip=True)
-        print(bodyContents)
-        f = open("articles/" + today + "/" + "(" + today + ")" + header + ".txt", 'w')
-        f.write(bodyContents)
-        f.close()
-        #bot.sendMessage(chat_id = util.my_chat_id, text = 'land.naver.com' + url)
+    for url in links_with_text:
+        if "/news/newsRead.nhn?type=headline" in url:
+            print('land.naver.com' + url)
+            page = requests.get('http://land.naver.com' + url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            header = soup.find('div', {'class' : 'article_header'}).find('h3').get_text(strip=True)
+            print(header)
+            bodyContents = soup.find('div', {'id' : 'articleBody'}).get_text(strip=True)
+            print(bodyContents)
+            f = open("articles/" + today + "/" + "(" + today + ")" + header + ".txt", 'w')
+            f.write(bodyContents)
+            f.close()
+            send_telegram('land.naver.com' + url)
 
 
 
 #==============================Finance News Notifier ==============================
-
 #Today's finance news. It's gonna be called in 08:00 every morning. (Except to weekend)
-
 #for i in range(0,10):
-partialUrl = "http://finance.naver.com/news/mainnews.nhn?date=" + today + "&page=" + "1"
-print(partialUrl)
-response = urllib.urlopen(partialUrl)
-soup = BeautifulSoup(response, 'html.parser')
-links_with_text = []
+def finance_news_notifier():
+    dt = datetime.datetime.now()
+    today = dt.strftime("%Y%m%d")
+    if not os.path.exists('articles/' + today):
+        os.makedirs('articles/' + today)
 
-for a in soup.find_all('a', href=True):
-    if a.text:
-        links_with_text.append(a['href'])
+    partialUrl = "http://finance.naver.com/news/mainnews.nhn?date=" + today + "&page=" + "1"
+    print(partialUrl)
+    response = urllib.urlopen(partialUrl)
+    soup = BeautifulSoup(response, 'html.parser')
+    links_with_text = []
 
-#bot.sendMessage(chat_id = util.my_chat_id, text = "Hello. "+ "Belows are " + today + "'s naver finance news.")
+    for a in soup.find_all('a', href=True):
+        if a.text:
+            links_with_text.append(a['href'])
 
-for url in links_with_text:
-    if "/news/news_read.nhn?article_id=" in url and "type=&date" in url:
-        print('finance.naver.com' + url)
-        page = requests.get('http://finance.naver.com' + url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        header = soup.find('div', {'class' : 'article_header'}).find('h3').get_text(strip=True)
-        print(header)
-        bodyContents = soup.find('div', {'id' : 'content'}).get_text(strip=True)
-        print(bodyContents)
-        f = open("articles/" + today + "/" + "(" + today + ")" + header + ".txt", 'w')
-        f.write(bodyContents)
-        f.close()
-        #bot.sendMessage(chat_id = util.my_chat_id, text='finance.naver.com' + url)
+    send_telegram("Hello. "+ "Belows are " + today + "'s naver finance news.")
+
+    for url in links_with_text:
+        if "/news/news_read.nhn?article_id=" in url and "type=&date" in url:
+            print('finance.naver.com' + url)
+            page = requests.get('http://finance.naver.com' + url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            header = soup.find('div', {'class' : 'article_header'}).find('h3').get_text(strip=True)
+            print(header)
+            bodyContents = soup.find('div', {'id' : 'content'}).get_text(strip=True)
+            print(bodyContents)
+            f = open("articles/" + today + "/" + "(" + today + ")" + header + ".txt", 'w')
+            f.write(bodyContents)
+            f.close()
+            send_telegram('finance.naver.com' + url)
+
+#==============================Sender to telegram==============================
+def send_telegram(msg):
+    bot = telegram.Bot(token = util.my_token)
+    bot.sendMessage(chat_id = util.my_chat_id, text = msg)
+    print('success sending message')
+
+
+if __name__ == "__main__":
+    stock_notifier()
+    finance_news_notifier()
+    land_news_notifier()
+else:
+    print('else')
